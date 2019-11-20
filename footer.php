20ef44329db65da6dacbb1ff7c27a589933dfda9
Patricia <pat.fargetti@gmail.com>
2019-11-19 16:53:55 -0800

    page style

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer.
  *
@@ -7,16 +8,25 @@
 
 ?>
 
-			</div><!-- #content -->
+</div><!-- #content -->
 
-			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
-				</div><!-- .site-info -->
-			</footer><!-- #colophon -->
-		</div><!-- #page -->
+<footer id="colophon" class="site-footer" role="contentinfo">
 
-		<?php wp_footer(); ?>
 
-	</body>
+	<div class="footer-inner">
+		<nav id="site-navigation" class="main-navigation" role="navigation">
+			<?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu')); ?>
+		</nav><!-- #site-navigation -->
+		<div class="site-info">
+			Brought to you by <a href="<?php echo esc_url('https://wordpress.org/'); ?>">REDAcademy</a>
+		</div><!-- .site-info -->
+	</div>
+
+</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+
 </html>
